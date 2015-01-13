b87d3c112aac860b2569a76d3883bed722395de8
philopon <philopon.dependence@gmail.com>
2015-01-13 19:39:49 +0900

    use Data.Function

--- src/Browser/WebStorage.purs
+++ src/Browser/WebStorage.purs
@@ -13,6 +13,7 @@ module Browser.WebStorage
   ) where
 
   import Data.Maybe
+  import Data.Function
 
   class Storage s where
     clear :: s -> s
@@ -24,18 +25,18 @@ module Browser.WebStorage
 
   instance storageLocalStorage :: Storage LocalStorage where
     length _ = unsafeLength localStorage
-    key _ = unsafeKey localStorage
-    getItem _ = unsafeGetItem localStorage
-    setItem _ = unsafeSetItem localStorage
-    removeItem _ = unsafeRemoveItem localStorage
+    key _ n = runFn2 unsafeKey localStorage n
+    getItem _ k = runFn2 unsafeGetItem localStorage k
+    setItem _ k v = runFn3 unsafeSetItem localStorage k v
+    removeItem _ k = runFn2 unsafeRemoveItem localStorage k
     clear _ = unsafeClear localStorage
 
   instance storageSessionStorage :: Storage SessionStorage where
     length _ = unsafeLength sessionStorage
-    key _ = unsafeKey sessionStorage
-    getItem _ = unsafeGetItem sessionStorage
-    setItem _ = unsafeSetItem sessionStorage
-    removeItem _ = unsafeRemoveItem sessionStorage
+    key _ n = runFn2 unsafeKey sessionStorage n
+    getItem _ k = runFn2 unsafeGetItem sessionStorage k
+    setItem _ k v = runFn3 unsafeSetItem sessionStorage k v
+    removeItem _ k = runFn2 unsafeRemoveItem sessionStorage k
     clear _ = unsafeClear sessionStorage
 
   foreign import data LocalStorage :: *
@@ -52,36 +53,26 @@ module Browser.WebStorage
     \}" :: forall storage. storage -> Number
 
   foreign import unsafeKey
-    "function unsafeKey(storage) {\
-    \  return function(num) {\
-    \    return null2Maybe(storage.key(num));\
-    \  }\
-    \}" :: forall storage. storage -> Number -> Maybe String
+    "function unsafeKey(storage,num) {\
+    \  return null2Maybe(storage.key(num));\
+    \}" :: forall storage. Fn2 storage Number (Maybe String)
 
   foreign import unsafeGetItem
-    "function unsafeGetItem(storage) {\
-    \  return function(str) {\
-    \    return null2Maybe(storage.getItem(str));\
-    \  }\
-    \}" :: forall storage v. storage -> String -> Maybe v
+    "function unsafeGetItem(storage,str) {\
+    \  return null2Maybe(storage.getItem(str));\
+    \}" :: forall storage v. Fn2 storage String (Maybe v)
 
   foreign import unsafeSetItem
-    "function unsafeSetItem(storage) {\
-    \  return function(str) {\
-    \    return function(val) {\
-    \      storage.setItem(str, val);\
-    \      return storage;\
-    \    }\
-    \  }\
-    \}" :: forall storage v. storage -> String -> v -> storage
+    "function unsafeSetItem(storage,str,val) {\
+    \  storage.setItem(str, val);\
+    \  return storage;\
+    \}" :: forall storage v. Fn3 storage String v storage
 
   foreign import unsafeRemoveItem
-    "function unsafeRemoveItem(storage) {\
-    \  return function(str) {\
-    \    storage.removeItem(str);\
-    \    return storage;\
-    \  }\
-    \}" :: forall storage. storage -> String -> storage
+    "function unsafeRemoveItem(storage,str) {\
+    \  storage.removeItem(str);\
+    \  return storage;\
+    \}" :: forall storage. Fn2 storage String storage
 
   foreign import unsafeClear
     "function unsafeClear(storage) {\
