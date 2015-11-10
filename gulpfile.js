77100cf6e875fc69e3d3ead3b6f7e1431f1993fe
joneshf <jones3.hardy@gmail.com>
2015-11-10 07:21:56 -0800

    Updated to latest things

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,81 @@
 'use strict'
 
-var gulp       = require('gulp')
-  , purescript = require('gulp-purescript')
+var gulp        = require('gulp')
+  , bump        = require('gulp-bump')
+  , filter      = require('gulp-filter')
+  , git         = require('gulp-git')
+  , purescript  = require('gulp-purescript')
+  , runSequence = require('run-sequence')
+  , tagVersion  = require('gulp-tag-version')
   ;
 
-var paths = {
-    src: [ 'src/**/*.purs'
-         , 'bower_components/purescript-*/src/**/*.purs'
-         ]
-  , ffi: [ 'src/**/*.js'
-         , 'bower_components/purescript-*/src/**/*.js'
-         ]
-  }
+var paths =
+    { src: 'src/**/*.purs'
+    , ffi: 'src/**/*.js'
+    , bowerFFIJs: [ 'bower_components/purescript-*/src/**/*.js'
+                  ]
+    , bowerSrc: [ 'bower_components/purescript-*/src/**/*.purs'
+                ]
+    , dest: ''
+    , docgen: { 'Browser.WebStorage': 'docs/Browser/WebStorage.md'
+              }
+    , outputJs: 'output/**/*.js'
+    };
 
-gulp.task('compile', function() {
+gulp.task('tag', function() {
+    return gulp.src(['bower.json', 'package.json'])
+        .pipe(git.commit('Update versions.'))
+        .pipe(filter('bower.json'))
+        .pipe(tagVersion());
+});
+
+// For whatever reason, these cannot be factored out...
+gulp.task('bump-major', function() {
+    return gulp.src(['bower.json', 'package.json'])
+        .pipe(bump({type: 'major'}))
+        .pipe(gulp.dest('./'));
+});
+gulp.task('bump-minor', function() {
+    return gulp.src(['bower.json', 'package.json'])
+        .pipe(bump({type: 'minor'}))
+        .pipe(gulp.dest('./'));
+});
+gulp.task('bump-patch', function() {
+    return gulp.src(['bower.json', 'package.json'])
+        .pipe(bump({type: 'patch'}))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump-tag-major', function() {
+    return runSequence('bump-major', 'tag');
+});
+gulp.task('bump-tag-minor', function() {
+    return runSequence('bump-minor', 'tag');
+});
+gulp.task('bump-tag-patch', function() {
+    return runSequence('bump-patch', 'tag');
+});
+
+gulp.task('psc', function() {
     return purescript.psc({
-      src: paths.src
-    , ffi: paths.ffi
+        src: paths.bowerSrc.concat(paths.src),
+        ffi: paths.bowerFFIJs.concat(paths.ffi)
     });
 });
 
 gulp.task('docs', function() {
     return purescript.pscDocs({
-      src: paths.src
-    , docgen: "Browser.WebStorage"
-    }).pipe(gulp.dest('README.md'));
+        src: paths.bowerSrc.concat(paths.src),
+        docgen: paths.docgen
+    });
 });
 
-gulp.task('default', ['compile', 'docs']);
+gulp.task('watch', function() {
+    gulp.watch([paths.src], function() {
+      return runSequence('psc', 'docs')
+    });
+});
+
+gulp.task('default', function() {
+  return runSequence('psc', 'docs')
+});
